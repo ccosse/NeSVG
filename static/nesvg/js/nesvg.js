3375b0200774be4f56b1eb88ba4b5bacaef00b2b
Charles Cosse <ccosse@gmail.com>
2017-07-14 12:58:49 -0600

    support recursive embedding with /static

--- static/nesvg/js/nesvg.js
+++ static/nesvg/js/nesvg.js
@@ -1,5 +1,5 @@
 var NeSVG=function(doneCB){
-	me={}
+	var me={}
 	me.doneCB=null;
 
 	if(doneCB)
@@ -7,7 +7,7 @@ var NeSVG=function(doneCB){
 	else{
 		me.doneCB=function(){
 			d3.select("#nesvg_main").style('display','none');
-			colormyworld.update_styles();
+//			MAYBE_SOMEBODY.update_styles();
 		}
 	}
 	me.show=function(){
@@ -21,18 +21,37 @@ var NeSVG=function(doneCB){
 			.style('position','absolute')
 			.style('left',parseInt(window.innerWidth/2-400)+"px");
 	}
+
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		//http://forums.mozillazine.org/viewtopic.php?f=25&t=1134615
+		try{
 		if (xhttp.readyState == 4 && (xhttp.status == 200 || xhttp.status == 0)) {
+			//http://forums.mozillazine.org/viewtopic.php?f=25&t=1134615
+			console.log("success importing base.html");
 			document.body.innerHTML+=xhttp.responseText;//base_html
+			console.log("nesvg.appended",xhttp.responseText)
 		}
+		else{
+			console.log("don't load this ... would result in multiple appends");
 //		else document.body.innerHTML+=base_html;
+		}
+		}catch(e){console.log(e)}
 	};
 	//https://stackoverflow.com/questions/7374911/reading-file-with-xmlhttprequest-results-in-error-console-entry
 	xhttp.overrideMimeType('text/plain');
-	xhttp.open("GET", "./static/nesvg/js/base.html", true);//test for localhost? ./static won't work on server.
+	xhttp.open("GET", "/static/NeSVG/static/nesvg/js/base.html", true);//test for localhost? ./static won't work on server.
 	xhttp.send();
+
+	cst=new ColorCfg();
+
+	me.cstCB=function(){
+		console.log('me.cstCB')
+	}
+	me.pick3CB=function(){
+		console.log("nesvg.pick3CB")
+		cst.show(me.cstCB);
+	}
+
 
 	return me;
 }
